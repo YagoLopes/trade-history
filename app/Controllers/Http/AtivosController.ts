110ed7e2b6fe93dba2adb1602cc227addf356c47
YagoLopes <yagolopeslazaro@gmail.com>
2021-02-28 12:43:47 -0300

    correcoes de diretorios

--- app/Controllers/Http/AtivosController.ts
+++
@@ -1,8 +0,0 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class AtivosController {
- 
-    public async index () {
-        return [{ id: 1, username: 'virk' }]
-      }
-}
